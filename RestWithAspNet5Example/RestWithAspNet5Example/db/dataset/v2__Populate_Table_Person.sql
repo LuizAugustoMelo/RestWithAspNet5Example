765cd0b691482898ea00bf4a453d16e1a2c43dfd
Luiz Augusto Melo <schaum62@hotmail.com>
2022-12-19 22:59:42 -0300

    Add Evolve sql migrations files

---
+++ RestWithAspNet5Example/RestWithAspNet5Example/db/dataset/v2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `person` (`Id`, `FirstName`, `LastName`, `Address`, `Gender`) VALUES
+	(1, 'Ayrton', 'Senna', 'São Paulo - Brasil', 'Male'),
+	(2, 'Leonardo', 'da Vince', 'Anchiano - Italy', 'Male'),
+	(3, 'Mahatma', 'Gandhi', 'Porbandar - India', 'Male'),
+	(4, 'Mohamed', 'Ali', 'Kentucky - USA', 'Male'),
+	(5, 'Nelson', 'Mandela', 'Mvezo - South Africa', 'Male');
---
+++ RestWithAspNet5Example/RestWithAspNet5Example/db/migrations/v3__Create_Table_Books.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS `books` (
+  `Id` bigint NOT NULL AUTO_INCREMENT,
+  `Author` longtext NOT NULL,
+  `Launch_Date` datetime(6) NOT NULL,
+  `Price` decimal(65,2) NOT NULL,
+  `Title` longtext NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_UNIQUE` (`Id`)
+)
