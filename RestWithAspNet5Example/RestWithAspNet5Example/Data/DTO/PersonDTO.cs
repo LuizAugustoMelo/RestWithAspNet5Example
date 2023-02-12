bfc26c99f7babef321c513e3c0a962bdbee9a59b
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-12 18:43:56 -0300

    Example of Json notations to use.

--- RestWithAspNet5Example/RestWithAspNet5Example/Data/DTO/PersonDTO.cs
+++ RestWithAspNet5Example/RestWithAspNet5Example/Data/DTO/PersonDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace RestWithAspNet5Example.Data.DTO
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RestWithAspNet5Example.Data.DTO
 {
     public class PersonDTO
     {
+        /*[JsonPropertyOrder(-5)]
+        [JsonIgnore] // Used to ignore a field
+        [JsonPropertyName("code")]//*/
         public long Id { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Address { get; set; }
+
         public string Gender { get; set; }
         
     }
