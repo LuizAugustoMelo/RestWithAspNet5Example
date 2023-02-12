0ec8e9af3dfddeb38d7483d5dc267d8e4cf4ff2a
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-12 17:47:44 -0300

    Creating Person Converter

---
+++ RestWithAspNet5Example/RestWithAspNet5Example/Data/Converter/Implementation/PersonConverter.cs
@@ -0,0 +1,53 @@
+﻿using RestWithAspNet5Example.Data.Converter.Contract;
+using RestWithAspNet5Example.Data.DTO;
+using RestWithAspNet5Example.Model;
+using System.Net;
+using System.Reflection;
+
+namespace RestWithAspNet5Example.Data.Converter.Implementation
+{
+    public class PersonConverter : IParser<PersonDTO, Person>, IParser<Person, PersonDTO>
+    {
+        public Person? Parse(PersonDTO origin)
+        {
+            if (origin == null) return null;
+
+            return new Person
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public PersonDTO? Parse(Person origin)
+        {
+            if (origin == null) return null;
+
+            return new PersonDTO
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<Person?> Parse(List<PersonDTO> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<PersonDTO?> Parse(List<Person> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
