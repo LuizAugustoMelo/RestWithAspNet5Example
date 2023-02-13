c502eb53b0a72aa088cac2bbafeba38786f7bf61
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-12 21:52:10 -0300

    Create BookEnricher class

---
+++ RestWithAspNet5Example/RestWithAspNet5Example/Hypermedia/Enricher/BookEnricher.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithAspNet5Example.Data.DTO;
+using RestWithAspNet5Example.Hypermedia.Constants;
+using System.Text;
+
+namespace RestWithAspNet5Example.Hypermedia.Enricher
+{
+    public class BookEnricher : ContentResponseEnricher<BookDTO>
+    {
+        private readonly object _lock = new object();
+
+        protected override Task EnrichModel(BookDTO content, IUrlHelper urlHelper)
+        {
+            var path = "api/persons/v1";
+            string link = GetLink(content.Id, urlHelper, path);
+
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultGet
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPut
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = link,
+                Rel = RelationType.self,
+                Type = "int"
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PATCH,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPatch
+            });
+
+            return null;
+        }
+
+        private string GetLink(long id, IUrlHelper urlHelper, string path)
+        {
+            lock (_lock)
+            {
+                var url = new { controller = path, id = id };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).ToString().ToLower().Replace("%2f", "/");
+            }
+        }
+    }
+}
