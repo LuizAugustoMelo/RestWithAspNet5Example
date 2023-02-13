839ab463e9125d65ab7bbd8e32dfd5d30cb5dcb7
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-12 21:37:25 -0300

    Adding base of HATEOAS

---
+++ RestWithAspNet5Example/RestWithAspNet5Example/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RestWithAspNet5Example.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutingContext context);
+
+        Task Enrich(ResultExecutingContext context);
+    }
+}
---
+++ RestWithAspNet5Example/RestWithAspNet5Example/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using RestWithAspNet5Example.Hypermedia.Abstract;
+
+namespace RestWithAspNet5Example.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
