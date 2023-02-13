2962f3f8179ab62dac32b63e1f793cf787b1c7ad
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-12 23:27:56 -0300

    Fixing CanEnrich method

--- RestWithAspNet5Example/RestWithAspNet5Example/Hypermedia/ContentResponseEnricher.cs
+++ RestWithAspNet5Example/RestWithAspNet5Example/Hypermedia/ContentResponseEnricher.cs
@@ -13,7 +13,7 @@ namespace RestWithAspNet5Example.Hypermedia
 
         }
 
-        public bool CanEnrich(Type contentType)
+        public virtual bool CanEnrich(Type contentType)
         {
             return contentType == typeof(T) || contentType == typeof(List<T>);
         }
