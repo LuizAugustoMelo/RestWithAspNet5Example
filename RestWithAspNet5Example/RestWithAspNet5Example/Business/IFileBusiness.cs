a739f2d997a7d8402c3b92a81fd3ee178564c7ca
Luiz Augusto Melo <schaum62@hotmail.com>
2023-02-19 13:33:10 -0300

    add upload files

---
+++ RestWithAspNet5Example/RestWithAspNet5Example/Business/IFileBusiness.cs
@@ -0,0 +1,11 @@
+﻿using RestWithAspNet5Example.Data.DTO;
+
+namespace RestWithAspNet5Example.Business
+{
+    public interface IFileBusiness
+    {
+        byte[] GetByte(string fileName);
+        Task<FileDetailDTO> SaveFileToDisk(IFormFile file);
+        Task<List<FileDetailDTO>> SaveFilesToDisk(List<IFormFile> files);
+    }
+}
